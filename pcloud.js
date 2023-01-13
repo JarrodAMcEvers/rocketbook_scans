d98d87ae0e6b926f2eaeb80f7592c7f36519175f
Jarrod McEvers <jarrodamcevers@gmail.com>
2023-01-12 21:05:04 -0600

    After authing, get the query params after pCloud redirects back to our web app on localhost.

--- pcloud.js
+++ pcloud.js
@@ -1,26 +1,40 @@
 const axios = require('axios');
+const { redirect } = require('express/lib/response');
 const puppeteer = require('puppeteer');
 const PCLOUD_OAUTH_ENDPOINT = 'https://my.pcloud.com/oauth2/authorize';
 
-exports.getAccessToken = async () => {
+exports.authWithPCloud = async () => {
     console.log('Launching browser.');
     const browser = await puppeteer.launch({ headless: false });
     console.log('Launching new page.');
     const page = await browser.newPage();
     console.log('Going to pCloud OAuth2 endpoint.');
     await page.goto(`${PCLOUD_OAUTH_ENDPOINT}?client_id=${process.env.CLIENT_ID}&response_type=token&redirect_uri=http://localhost:8080`);
+    console.log('Entering credentials.');
+    await page.waitForSelector('input[name=email]');
     await page.focus('input[name=email]');
-    await page.keyboard.type('');
+    await page.keyboard.type('jarrodamcevers@gmail.com');
     await page.click('button[type=submit]');
     await page.waitForSelector('input[name=password]');
     await page.focus('input[name=password]');
-    await page.keyboard.type('');
+    await page.keyboard.type('unxh9FKKye*m*6CN3GoAsNrv4tQH3Jy9dB7iNG');
+    console.log('Logging in.');
     await page.click('button[type=submit]');
     await page.waitForNavigation();
 
-    console.log(await page.url())
+    const redirectURL = await page.url();
+    const arrayOfParams = redirectURL.split('#')[1].split('&');
+    const queryParams = {};
+    for (let param of arrayOfParams) {
+        let parts = param.split('=');
+        queryParams[parts[0]] = parts[1];
+    }
+
+    // close the browser last as the redirectURL changes if it happens before creating the dictionary of query params
     console.log('Closing browser.')
     await browser.close();
+
+    return queryParams;
 }
 
-exports.getAccessToken()
+exports.authWithPCloud()
