d1d45c7157d837ebaa0b94963f528a5c76986295
Jarrod McEvers <jarrodamcevers@gmail.com>
2023-02-02 21:57:15 -0600

    Add axios HTTP library. Create class to encapsulate auth response for making API calls to pCloud.

--- src/pcloud.js
+++ src/pcloud.js
@@ -1,27 +1,43 @@
+const axios = require('axios');
 const puppeteer = require('puppeteer');
 const PCLOUD_OAUTH_ENDPOINT = 'https://my.pcloud.com/oauth2/authorize';
 const REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8080'
+
 class pCloud {
     constructor(apiHost, accessToken, userID) {
         self.apiHost = `https://${apiHost}`;
         self.accessToken = accessToken;
         self.userID = userID;
-        self.folderID = self.getFolderID();
     }
 
-    async getFolderID() {
-        const response = await axios.get(`${self.apiHost}/listfolder?access_token=${self.accessToken}&path=${PCLOUD_PATH}`)
-        return response.metadata.folderid;
+    fetchFolderId = async (pCloudPath) => {
+        const response = await axios.get(`${self.apiHost}/listfolder?access_token=${self.accessToken}&path=${pCloudPath}`);
+        self.folderId = response.metadata.folderid;
+    }
+
+    uploadFileToFolder = async (filePath) => {
+        let formData = new FormData();
+        formData.append('file', filePath);
+        const response = await axios.post(
+            `${self.apiHost}/uploadfile?access_token=${self.accessToken}&folderid=${self.folderID}`,
+            formData,
+            { headers: { 'Content-Type': 'multipart/form-data' } }
+        );
+        // TODO: figure out what the response body looks like, probably just a 202 or 200
+        return response;
     }
 }
 
 exports.authWithPCloud = async () => {
     console.log('Launching browser.');
     const browser = await puppeteer.launch({ headless: false });
+
     console.log('Launching new page.');
     const page = await browser.newPage();
+
     console.log('Going to pCloud OAuth2 endpoint.');
     await page.goto(`${PCLOUD_OAUTH_ENDPOINT}?client_id=${process.env.CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}`);
+
     console.log('Entering credentials.');
     await page.waitForSelector('input[name=email]');
     await page.focus('input[name=email]');
@@ -30,6 +46,7 @@ exports.authWithPCloud = async () => {
     await page.waitForSelector('input[name=password]');
     await page.focus('input[name=password]');
     await page.keyboard.type('unxh9FKKye*m*6CN3GoAsNrv4tQH3Jy9dB7iNG');
+
     console.log('Logging in.');
     await page.click('button[type=submit]');
     await page.waitForNavigation();
@@ -49,4 +66,4 @@ exports.authWithPCloud = async () => {
     return queryParams;
 }
 
-exports.authWithPCloud()
+exports.authWithPCloud();
