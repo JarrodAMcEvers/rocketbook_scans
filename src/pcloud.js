64ec42892c0745ef3e10f1dd73c1c39cc168a137
Jarrod McEvers <jarrodamcevers@gmail.com>
2023-04-04 10:11:29 -0500

    Move auth code into class. Make pCloud constructor take no args as authWithPCloud will take care of everything.

--- src/pcloud.js
+++ src/pcloud.js
@@ -5,13 +5,65 @@ const PCLOUD_OAUTH_ENDPOINT = 'https://my.pcloud.com/oauth2/authorize';
 const REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8080'
 
 class pCloud {
-    constructor(apiHost, accessToken, userID) {
-        this.apiHost = `https://${apiHost}`;
-        this.accessToken = accessToken;
-        this.userID = userID;
+    apiHost;
+    accessToken;
+    userID;
+
+    constructor() { }
+
+    authWithPCloud = async () => {
+        console.log('Launching browser.');
+        const browser = await puppeteer.launch({ headless: false });
+
+        console.log('Launching new page.');
+        const page = await browser.newPage();
+
+        console.log('Going to pCloud OAuth2 endpoint to authenticate.');
+        await page.goto(`${PCLOUD_OAUTH_ENDPOINT}?client_id=${process.env.CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}`);
+
+        console.log('Entering credentials.');
+        await page.waitForSelector('input[name=email]');
+        await page.focus('input[name=email]');
+        await page.keyboard.type('jarrodamcevers@gmail.com');
+        await page.click('button[type=submit]');
+        await page.waitForSelector('input[name=password]');
+        await page.focus('input[name=password]');
+        await page.keyboard.type('unxh9FKKye*m*6CN3GoAsNrv4tQH3Jy9dB7iNG');
+
+        console.log('Logging in.');
+        await page.click('button[type=submit]');
+        await page.waitForNavigation();
+
+        const redirectURL = await page.url();
+        const responseQueryParams = redirectURL.split('#')[1].split('&');
+        this.parseAndLoadResponseQueryParameters(responseQueryParams);
+
+        // close the browser last as the redirectURL changes if it happens before creating the dictionary of query params
+        console.log('Closing browser.')
+        await browser.close();
     }
 
-    fetchFolderId = async (pCloudPath) => {
+    parseAndLoadResponseQueryParameters(responseQueryParams) {
+        console.log('All of the response query parameters: ', responseQueryParams);
+        for (let param of responseQueryParams) {
+            let parts = param.split('=');
+            switch (parts[0]) {
+                case 'hostname':
+                    this.apiHost = 'https://' + parts[1]
+                    break;
+                case 'access_token':
+                    this.accessToken = parts[1]
+                    break;
+                case 'userid':
+                    this.userID = parts[1];
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    fetchAndSetFolderId = async (pCloudPath) => {
         const response = await axios.get(`${this.apiHost}/listfolder?access_token=${this.accessToken}&path=${pCloudPath}`);
         this.folderID = response.data.metadata.folderid;
     }
@@ -28,40 +80,4 @@ class pCloud {
     }
 }
 
-exports.authWithPCloud = async () => {
-    console.log('Launching browser.');
-    const browser = await puppeteer.launch({ headless: false });
-
-    console.log('Launching new page.');
-    const page = await browser.newPage();
-
-    console.log('Going to pCloud OAuth2 endpoint.');
-    await page.goto(`${PCLOUD_OAUTH_ENDPOINT}?client_id=${process.env.CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}`);
-
-    console.log('Entering credentials.');
-    await page.waitForSelector('input[name=email]');
-    await page.focus('input[name=email]');
-    await page.keyboard.type('jarrodamcevers@gmail.com');
-    await page.click('button[type=submit]');
-    await page.waitForSelector('input[name=password]');
-    await page.focus('input[name=password]');
-    await page.keyboard.type('unxh9FKKye*m*6CN3GoAsNrv4tQH3Jy9dB7iNG');
-
-    console.log('Logging in.');
-    await page.click('button[type=submit]');
-    await page.waitForNavigation();
-
-    const redirectURL = await page.url();
-    const arrayOfParams = redirectURL.split('#')[1].split('&');
-    const queryParams = {};
-    for (let param of arrayOfParams) {
-        let parts = param.split('=');
-        queryParams[parts[0]] = parts[1];
-    }
-
-    // close the browser last as the redirectURL changes if it happens before creating the dictionary of query params
-    console.log('Closing browser.')
-    await browser.close();
-
-    return queryParams;
-}
+module.exports = pCloud
